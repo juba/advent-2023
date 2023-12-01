d35b42f05997ba560f419186c70dd140d04e4088
Julien Barnier <julien@nozav.org>
2023-12-02 00:13:46 +0100

    01: replace convert function with get(v, v)

--- days/01.py
+++ days/01.py
@@ -13,22 +13,19 @@ def puzzle1(input_data):
 
 def puzzle2(input_data):
     lines = input_data.splitlines()
-    alpha_digits = dict(
-        zip(
+    alpha = dict(
+        zip(  # noqa: B905
             ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
             range(1, 10),
-            strict=True,
         )
     )
 
-    convert = (  # noqa: E731
-        lambda d: int(alpha_digits[d]) if d in alpha_digits else int(d)
-    )
-
-    pattern = f"(\\d|{'|'.join(alpha_digits.keys())})"
+    pattern = f"(\\d|{'|'.join(alpha.keys())})"
     tens = [re.findall(f"^.*?{pattern}", line)[0] for line in lines]
     units = [re.findall(f"^.*{pattern}.*?$", line)[0] for line in lines]
-    digits = [convert(ten) * 10 for ten in tens] + [convert(unit) for unit in units]
+    digits = [int(alpha.get(ten, ten)) * 10 for ten in tens] + [
+        int(alpha.get(unit, unit)) for unit in units
+    ]
     return sum(digits)
 
 
